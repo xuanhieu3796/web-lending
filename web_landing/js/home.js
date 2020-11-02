1c2d950cfa114f1b21c396f03eb7d789792e792c
{Hieu} <{xuanhieu3796@mail.com}>
2020-11-02 07:07:10 +0700

    d

---
+++ web_landing/js/home.js
@@ -0,0 +1,11 @@
+$('.silder-element').owlCarousel({
+    loop:true,
+    stopOnHover:true,
+    margin:10,
+    Timeout:10000,   
+    itemsCustom: [
+        [0, 1],
+        [768, 1],
+        [992, 1],
+    ],
+})
