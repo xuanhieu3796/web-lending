57f1c91900fd9f10d0be0664353f9a535f648ad0
{Hieu} <{xuanhieu3796@mail.com}>
2020-11-03 06:51:21 +0700

    nh

--- web_landing/js/home.js
+++ web_landing/js/home.js
@@ -1,11 +1,13 @@
-$('.silder-element').owlCarousel({
-    loop:true,
-    stopOnHover:true,
-    margin:10,
-    Timeout:10000,   
-    itemsCustom: [
-        [0, 1],
-        [768, 1],
-        [992, 1],
-    ],
-})
+$(document).ready(function() {
+ 
+  $(".silder-element").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1]
+ 
+  });
+ 
+});
